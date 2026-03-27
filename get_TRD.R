323e8af2e5646703f6952b094b778689329ac84b
Katie Forthman <kforthman@submit0.c3.laureateinstitute.org>
2026-03-27 14:07:26 -0500

    Modifying get_TRD.R for SF data.

--- get_TRD.R
+++ get_TRD.R
@@ -1,77 +1,44 @@
 # %%
-source("workbench_functions.txt")
-source("easy_delete_copy_R.txt")
-source("data_prep_functions.txt")
-notebook_setup()
+mdd_first_record <- mdd_data %>% 
+select(PatientDurableKey, MDD_Index)
 
 # %%
-load("Data_Prepped/Prepped_Data-All_Participants-Depression-Condition_Occurrence.rds")
-mdd_record <- data_prep
-mdd_record %>% head
+antidepressant_record <- antidepressant_table  %>%
+filter(PatientDurableKey %in% mdd_first_record$PatientDurableKey) %>%
+left_join(mdd_first_record, by = join_by(PatientDurableKey == PatientDurableKey)) %>% 
+filter(MedicationStartDate >= MDD_Index) # Filter to drugs that are prescribed after first MDD diagnosis
 
 # %%
-mdd_first_record <- mdd_record %>% 
-group_by(person_id) %>% 
-filter(start_datetime == min(start_datetime)) %>% 
-dplyr::select(person_id, start_datetime) %>%
-rename(first_depression_dx_date = start_datetime) %>%
-slice(1) %>%
-ungroup()
-
-mdd_first_record %>% head
-mdd_first_record %>% nrow
-
-# %%
-load("Data_Prepped/Prepped_Data-All_Participants-Antidepressants-Drug_Exposure.rds")
-antidepressant_record <- data_prep  %>%
-filter(person_id %in% mdd_first_record$person_id) %>%
-left_join(mdd_first_record, by = join_by(person_id == person_id)) %>% # Filter to drugs that are prescribed after first MDD diagnosis
-filter(start_datetime >= first_depression_dx_date)
-
-antidepressant_record %>%
-relocate(c(concept_name_detail, start_datetime, end_datetime, first_depression_dx_date), .after = 2) %>% 
-head
-antidepressant_record %>% dim
-
-# %%
-antidepressant_record %>% head %>% print_all_cols
-
-# %% [markdown]
-# # Drug consecutive instance
-
-# %%
-"\n\nThere are " %>% cat
-antidepressant_record$person_id %>% 
-unique %>% 
-length %>%
-comma %>%
-cat
-" participants in the antidepressant drug dataset." %>% cat
-"\nThe dataset has " %>% cat
-antidepressant_record %>% nrow %>% comma %>% cat
-" antidepressant drug records." %>% cat
+# "\n\nThere are " %>% cat
+# antidepressant_record$PatientDurableKey %>% 
+# unique %>% 
+# length %>%
+# comma %>%
+# cat
+# " participants in the antidepressant drug dataset." %>% cat
+# "\nThe dataset has " %>% cat
+# antidepressant_record %>% nrow %>% comma %>% cat
+# " antidepressant drug records." %>% cat
 
 # %%
 # create a list of all drugs that each participant has taken
 antidepressant_record_uq <- antidepressant_record %>% 
-dplyr::select(person_id, concept_name_detail) %>% 
+dplyr::select(PatientDurableKey, SimpleGenericName) %>% 
 unique %>%
-arrange(person_id)
-
-antidepressant_record_uq %>% head
+arrange(PatientDurableKey)
 
 # %%
-filename <- "antidepressant_consecutive_instance.csv"
+filename <- "OutputData/antidepressant_consecutive_instance.csv"
 overwrite <- TRUE
 if(!file.exists(filename) | overwrite){
     df <- antidepressant_record
 
     # Calculate intervals and instances
     df <- df %>%
-      arrange(person_id, concept_name_detail, start_datetime) %>%
-      group_by(person_id, concept_name_detail) %>%
+      arrange(PatientDurableKey, SimpleGenericName, MedicationStartDate) %>%
+      group_by(PatientDurableKey, SimpleGenericName) %>%
       mutate(
-        interval = as.numeric(difftime(start_datetime, lag(start_datetime), units = "days")),
+        interval = as.numeric(difftime(MedicationStartDate, lag(MedicationStartDate), units = "days")),
         interval = ifelse(row_number() == 1, 1, interval),
         consecutive_instance = interval > 180,
         consecutive_instance = ifelse(row_number() == 1, TRUE, consecutive_instance),
@@ -80,18 +47,17 @@ if(!file.exists(filename) | overwrite){
 
     # Calculate first and last records
     consecutive_instance_tab <- df %>%
-      group_by(person_id, concept_name_detail, consecutive_instance) %>%
+      group_by(PatientDurableKey, SimpleGenericName, consecutive_instance) %>%
       summarise(
-        first.record = first(start_datetime),
-        last.record = last(start_datetime),
-        last.record.days_supply = last(days_supply),
-        drug_class = first(concept_category_level_4),
+        first_record = first(MedicationStartDate),
+        last_record = last(MedicationStartDate),
+        last_record_days_supply = last(DaysSupply),
+        drug_class = first(Category_Level_4),
         .groups = 'drop'
       ) %>%
       dplyr::select(-consecutive_instance) %>%
-      arrange(person_id, first.record, concept_name_detail) %>%
-      mutate(last.record = replace(last.record, last.record == first.record, as.Date(NA))) %>%
-      rename(person.id = person_id) %>%
+      arrange(PatientDurableKey, first_record, SimpleGenericName) %>%
+      mutate(last_record = replace(last_record, last_record == first_record, as.Date(NA))) %>%
       as.data.frame 
       consecutive_instance_tab %>% write.csv(filename, row.names = FALSE)
 }else{
@@ -99,26 +65,22 @@ if(!file.exists(filename) | overwrite){
 }
 
 # %%
-consecutive_instance_tab %>% head
-
-# %%
 overwrite <- TRUE
-my_progress <- 0
-filename <- "antidepressant_consecutive_period.csv"
+filename <- "OutputData/antidepressant_consecutive_period.csv"
 if(!file.exists(filename) | overwrite){
     assign_period <- function(this_data){
         for(i in 1:nrow(this_data)){
 
             this_period_time_from_start <- ifelse(i == 1, 
                                                   0, 
-                                                  as.integer(difftime(as_date(this_data$first.record[i]), 
+                                                  as.integer(difftime(as_date(this_data$first_record[i]), 
                                                                       as_date(this_data$period_start[i-1]), 
                                                                       units = "days"))
                                                  )
 
             new_period <- ifelse(i == 1, 
                    TRUE, 
-                   (this_period_time_from_start > 365 | (this_data$concept_name_detail[i] == this_data$concept_name_detail[i-1]))
+                   (this_period_time_from_start > 365 | (this_data$SimpleGenericName[i] == this_data$SimpleGenericName[i-1]))
                                  )
                                  
             this_data$period[i] = ifelse(i == 1, 
@@ -130,7 +92,7 @@ if(!file.exists(filename) | overwrite){
                                          )
 
             this_data$period_start[i] <- ifelse(new_period, 
-                                                this_data$first.record[i], 
+                                                this_data$first_record[i], 
                                                 this_data$period_start[i-1])
 
             this_data$period_time_from_start[i] <- ifelse(new_period, 
@@ -147,7 +109,7 @@ if(!file.exists(filename) | overwrite){
         period_start = NA,
         period_time_from_start = NA
     ) %>%
-    split(consecutive_instance_tab$person.id)
+    split(consecutive_instance_tab$PatientDurableKey)
 
     # Register the parallel backend
     num_cores <- detectCores()
@@ -169,57 +131,22 @@ if(!file.exists(filename) | overwrite){
 }
 
 # %%
-consecutive_period_tab %>% head(35)
-
-# %%
-consecutive_period_tab$drug_class %>% table
-consecutive_period_tab$drug_class %>% is.na %>% sum
-
-# %%
-write.csv(consecutive_period_tab, "antidepressant_consecutive_period_tab.csv")
-
-# %%
-consecutive_period_tab_summ <- consecutive_period_tab %>% group_by(person.id,period) %>% 
+consecutive_period_tab_summ <- consecutive_period_tab %>% group_by(PatientDurableKey, period) %>% 
 summarise(
-    ndrug = (concept_name_detail %>% unique %>% length),
-    drugs = (concept_name_detail %>% unique %>% paste(collapse = ", ")),
-    period.start = min(first.record), period.end = max(first.record))
-consecutive_period_tab_summ %>% head(10)
+    ndrug = (SimpleGenericName %>% unique %>% length),
+    drugs = (SimpleGenericName %>% unique %>% paste(collapse = ", ")),
+    period_start = min(first_record), 
+    period_end = max(first_record))
 
 # %%
-write.csv(consecutive_period_tab_summ, "antidepressant_consecutive_period_tab_summ.csv")
+write.csv(consecutive_period_tab_summ, "OutputData/antidepressant_consecutive_period_tab_summ.csv")
 
 # %%
-consecutive_period_maxDrugs <- consecutive_period_tab_summ %>% group_by(person.id) %>% 
+consecutive_period_maxDrugs <- consecutive_period_tab_summ %>% group_by(PatientDurableKey) %>% 
         filter(ndrug == max(ndrug)) %>% filter(period == max(period))
 consecutive_period_maxDrugs$has_TRD <- consecutive_period_maxDrugs$ndrug > 2
-write.csv(consecutive_period_maxDrugs, "antidepressant_consecutive_period_maxDrugs.csv")
-consecutive_period_maxDrugs
+write.csv(consecutive_period_maxDrugs, "OutputData/antidepressant_consecutive_period_maxDrugs.csv")
 
 # %%
-consecutive_period_maxDrugs$ndrug %>% table
-
-# %%
-cat(paste0("N TRD: ",(consecutive_period_maxDrugs$has_TRD) %>% sum, "\n"))
-cat(paste0("N Not TRD: ",(!consecutive_period_maxDrugs$has_TRD) %>% sum))
-
-# %%
-TRD_list <- consecutive_period_maxDrugs %>% filter(has_TRD) %>% dplyr::select(person.id)
-write.table(unique(TRD_list$person.id), "Data_Prepped/IDs-TRD.txt", row.names = F, col.names = F)
-
-# %%
-bpd_ids <- read.csv("Data_Prepped/IDs-Bipolar_Disorder-Condition_Occurrence.txt", header = F) %>% rename(person_id = V1)
-sch_ids <- read.csv("Data_Prepped/IDs-Schizophrenia-Condition_Occurrence.txt", header = F) %>% rename(person_id = V1)
-
-trd_id <- data.frame(person_id = unique(TRD_list$person.id))
-
-trd_no_bpd_sch_id <- data.frame(person_id = trd_id$person_id[!trd_id$person_id %in% c(bpd_ids$person_id, sch_ids$person_id)])
-cat(paste0("N TRD: ",(trd_no_bpd_sch_id) %>% nrow, "\n"))
-
-# %%
-v7_ids <- read.csv("IDs-All_IDs_v7.txt", header = F) %>% rename(person_id = V1)
-
-trd_no_bpd_sch_v7_id <- data.frame(person_id = trd_no_bpd_sch_id$person_id[trd_no_bpd_sch_id$person_id %in% v7_ids$person_id])
-cat(paste0("N TRD: ",(trd_no_bpd_sch_v7_id) %>% nrow, "\n"))
-
-# %%
+TRD_list <- consecutive_period_maxDrugs %>% filter(has_TRD) %>% dplyr::select(PatientDurableKey)
+write.csv(data.frame(PatientDurableKey = unique(TRD_list$PatientDurableKey)), "OutputData/IDs-TRD.csv", row.names = F)
