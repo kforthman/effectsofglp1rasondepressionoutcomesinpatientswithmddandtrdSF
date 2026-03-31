363de5eab85b19c1a10591742b67efe0497f843c
Katie Forthman <kforthman@submit0.c3.laureateinstitute.org>
2026-03-31 12:15:35 -0500

    Modifying plot-Antidiabetic_overlap.R for SF data.

--- plot-Antidiabetic_overlap.R
+++ plot-Antidiabetic_overlap.R
@@ -1,53 +1,28 @@
-# %%
-source("workbench_functions.txt")
-source("easy_delete_copy_R.txt")
-notebook_setup()
-
-# %%
-load("dte_cohort_wNontreat_data.rds", verbose = TRUE)
-
-# %%
-drug_info <- c("Semaglutide",
-                        "Insulins",
-                        "Metformin",
-                        "DPP4i",
-                        "SGLT2i",
-                        "SU",
-                        "TZD",
-                        "GLP1RA",
-                        "Nontreatment"
-                        ) %>%
-matrix(ncol = 1, byrow = TRUE) %>%
-as.data.frame(stringsAsFactors = FALSE) %>%
-setNames(c("drug"))
+this.data <- nontreat_result$dte_cohort_data2
+all_groups
 
 # %%
 # Loop over each drug to identify which drug comes first
-for(i in 1:nrow(drug_info)){
-    this_drug_1 <-  drug_info$drug[i]
-    for(j in 1:nrow(drug_info)){
-        this_drug_2 <-  drug_info$drug[j]
-        if(this_drug_1 == this_drug_2){next}
-        var_name_timeline <- paste0(this_drug_1, "_index_precedes_", this_drug_2, "_index")
-        var_name_first_drug_record <- ifelse(this_drug_1 == "Nontreatment", "Nontreatment_index", 
-                                             paste0(this_drug_1, "_first_drug_record"))
-        var_name_first_comp_record <- ifelse(this_drug_2 == "Nontreatment", "Nontreatment_index", 
-                                             paste0(this_drug_2, "_first_drug_record"))
-        
-        this.data <- this.data %>%
-        mutate(!!sym(var_name_timeline) := !!sym(var_name_first_drug_record) < !!sym(var_name_first_comp_record))
-    }
+for(i in 1:length(all_groups)){
+  this_drug_1 <-  all_groups[i]
+  for(j in 1:length(all_groups)){
+    this_drug_2 <-  all_groups[j]
+    if(this_drug_1 == this_drug_2){next}
+    var_name_timeline <- paste0(this_drug_1, "_index_precedes_", this_drug_2, "_index")
+    var_name_drug_1_index <- paste0(this_drug_1, "_Index")
+    var_name_drug_2_index <- paste0(this_drug_2, "_Index")
+    
+    this.data <- this.data %>%
+      mutate(!!sym(var_name_timeline) := !!sym(var_name_drug_1_index) < !!sym(var_name_drug_2_index))
+  }
 }
 
 # %%
 # Prepare your data
-use_matrix <- this.data %>% 
-filter(MDD_noBDnoSCH & 
-       Meets_Custom_Eligibility_Requirements) %>%
-  dplyr::select(Semaglutide_Use, Insulins_Use, Metformin_Use, 
-                DPP4i_Use, SGLT2i_Use, SU_Use, TZD_Use, GLP1RA_Use, Nontreatment_Population_for_Semaglutide_vs_Nontreatment) %>% 
+use_matrix <- this.data %>%
+  select(paste0(c(all_drugs, nontreatment_group), "_Index")) %>%
+  mutate(across(everything(), ~ if_else(is.na(.), 0L, 1L))) %>% 
   as.matrix
-use_matrix <- use_matrix * 1
 co_occurrence <- t(use_matrix) %*% use_matrix
 
 co_occurrence
@@ -60,8 +35,7 @@ par(mar = c(0,0,0,0))
 my_palette <- libr.pal(200, "LIBR_gradient_pink")[1:175]
 
 # Define custom axis titles (corresponding to the columns/rows in your data)
-axis_titles <- c("Semaglutide Use", "Insulin Use", "Metformin Use", 
-                 "DPP-4i Use", "SGLT-2i Use", "SU Use", "TZD Use", "Other GLP1-RA Use", "Nontreatment Group")
+axis_titles <- c(paste0(all_drugs, " Use"), paste0(nontreatment_group, " Group"))
 
 # Set row and column names of the co-occurrence matrix to match these titles
 rownames(co_occurrence) <- axis_titles
@@ -126,28 +100,25 @@ color.legend(legend_xl, legend_yb, legend_xr, legend_yt,
              family = "Poppins")
 
 # %%
-use_matrix <- matrix(NA, nrow = nrow(drug_info), ncol = nrow(drug_info))
-rownames(use_matrix) <- 1:nrow(drug_info)
-colnames(use_matrix) <- 1:nrow(drug_info)
+use_matrix <- matrix(NA, nrow = length(all_groups), ncol = length(all_groups))
+rownames(use_matrix) <- 1:length(all_groups)
+colnames(use_matrix) <- 1:length(all_groups)
 
-this.data_filt <- this.data %>% 
-filter(MDD_noBDnoSCH & 
-       Meets_Custom_Eligibility_Requirements)
-
-for(i in 1:nrow(drug_info)){
-    this_drug_1 <-  drug_info$drug[i]
-    rownames(use_matrix)[i] <- paste0(this_drug_1, " precedes")
-    for(j in 1:nrow(drug_info)){
-        this_drug_2 <-  drug_info$drug[j]
-        colnames(use_matrix)[j] <- this_drug_2
-        if(this_drug_1 == this_drug_2){
-            this_col <- ifelse(this_drug_1 == "Nontreatment", "Nontreatment_Population_for_Semaglutide_vs_Nontreatment", paste0(this_drug_1, "_Use"))
-            use_matrix[i,j] <- this.data_filt %>% pull(!!sym(this_col)) %>% sum(na.rm = T)
-            next
-        }
-        use_matrix[i,j] <- this.data_filt %>% pull(!!sym(paste0(this_drug_1, "_index_precedes_", this_drug_2, "_index"))) %>% sum(na.rm = T)
-        
+for(i in 1:length(all_groups)){
+  this_drug_1 <-  all_groups[i]
+  rownames(use_matrix)[i] <- paste0(this_drug_1, " precedes")
+  for(j in 1:length(all_groups)){
+    this_drug_2 <-  all_groups[j]
+    colnames(use_matrix)[j] <- this_drug_2
+    if(this_drug_1 == this_drug_2){
+      use_matrix[i,j] <- NA
+      next
     }
+    use_matrix[i,j] <- this.data %>% 
+      pull(!!sym(paste0(this_drug_1, "_index_precedes_", this_drug_2, "_index"))) %>% 
+      sum(na.rm = T)
+    
+  }
 }
 
 use_matrix
@@ -172,7 +143,7 @@ corrplot(use_matrix,
          mar = c(2, 0, 0, 8),  # (bottom, left, top, right)
          cl.pos = "n",         # suppress the built-in legend
          col = my_palette, 
-             family = "Poppins")
+         family = "Poppins")
 
 # Add the custom title with mtext() to move it lower
 mtext("Lifetime Co-occurrence Counts of Antidiabetic Medications", 
@@ -181,7 +152,7 @@ mtext("Lifetime Co-occurrence Counts of Antidiabetic Medications",
       adj = 0.75,
       cex = 2,    # Adjust title size
       col = "black", 
-             family = "Poppins")  # Set title color
+      family = "Poppins")  # Set title color
 
 # Format the counts with commas
 formatted_numbers <- apply(use_matrix, c(1,2), function(x) format(x, big.mark = ",", scientific = FALSE))
@@ -192,7 +163,7 @@ for (i in 1:n) {
   for (j in 1:n) {
     # Note: corrplot flips the y-axis.
     text(j, n - i + 1, formatted_numbers[i, j], col = "black", cex = 2, 
-             family = "Poppins")
+         family = "Poppins")
   }
 }
 
