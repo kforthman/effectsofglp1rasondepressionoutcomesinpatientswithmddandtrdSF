8fa1543696d0f9100fbaa1707e3221e8c38375e9
Katie Forthman <kforthman@submit0.c3.laureateinstitute.org>
2026-04-13 16:09:37 -0500

    Adding data prep to main script.

--- main.R
+++ main.R
@@ -43,6 +43,9 @@ drug_class  <- read.csv(config$files$drug_class)
 cpt_acuity  <- read.csv(config$files$cpt_acuity)
 period_info <- read.csv(config$files$period_info)
 
+# ── Prep Data ─────────────────────────────────────────────────────----
+source("prepData.R")
+
 # ── Identify TRD patients ─────────────────────────────────────────────────────----
 
 source("get_TRD.R")
